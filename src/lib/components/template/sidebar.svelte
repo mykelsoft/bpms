f419220a5bc70cc156e883ca2594e77f9240c107
Michael Jumoc <michaeljumoc@gmail.com>
2025-02-03 23:28:08 +0800

    Layout adjustments

--- src/lib/components/template/sidebar.svelte
+++ src/lib/components/template/sidebar.svelte
@@ -1,23 +1,38 @@
+<script lang="ts">
+	import HomeIcon from '$lib/icons/HomeIcon.svg?raw';
+
+	const links = [
+		{ name: 'Home', href: '/', icon: HomeIcon },
+		{ name: 'Dashboard', href: '/dashboard', icon: HomeIcon },
+		{ name: 'Team', href: '/team', icon: HomeIcon },
+		{ name: 'Projects', href: '/projects', icon: HomeIcon },
+		{ name: 'Calendar', href: '/calendar', icon: HomeIcon },
+		{ name: 'Documents', href: '/documents', icon: HomeIcon }
+	];
+</script>
+
 <aside
-	class="bg-brand-700 fixed top-0 left-0 z-10 h-full flex-auto translate-0 items-stretch outline-none sm:top-14 sm:left-0 sm:z-0 sm:flex sm:h-[calc(100%-56px)]">
+	class="bg-brand-700 fixed top-0 left-0 z-10 h-full flex-auto translate-0 items-stretch outline-none sm:top-14 sm:left-0 sm:z-0 sm:flex sm:h-[calc(100%-var(--header-height))]">
 	<div class="relative">
 		<nav
-			class="rounded-r-0 sm:rounded-tr-0 relative mt-14 flex h-full min-h-full w-full max-w-[360px] min-w-[240px] flex-col items-stretch rounded-tl-xl bg-gray-50 sm:mt-0 sm:max-w-[240px]">
+			class="rounded-r-0 sm:rounded-tr-0 relative mt-14 flex h-full min-h-full w-full max-w-[360px] min-w-[var(--sidebar-width)] flex-col items-stretch rounded-tl-xl bg-gray-50 sm:mt-0 sm:max-w-[var(--sidebar-width)]">
 			<div
-				class="sm:rounded-tr-0 scrollbar sm:rounded-tr-xl-0 isolate flex max-h-none w-full max-w-full flex-1 flex-col items-stretch overflow-auto supports-h-svh:h-[calc(100svh-56px-0)] sm:pt-3">
+				class="sm:rounded-tr-0 scrollbar sm:rounded-tr-xl-0 isolate flex max-h-none w-full max-w-full flex-1 flex-col items-stretch overflow-auto supports-h-svh:h-[calc(100svh-var(--header-height))] sm:pt-3">
 				<div class="space-y-3">
 					<ul class="sidebar-navigation-section">
-						<li class="sidebar-navigation-item">
-							<div class="sidebar-navigation-itemWrapper">
-								<div class="sidebar-navigation-itemInnerWrapper">
-									<a
-										href="/"
-										class="relative m-0 flex grow cursor-pointer items-start rounded-lg px-2 py-1 text-left text-sm font-semibold text-gray-600 no-underline">
-										Home
-									</a>
+						{#each links as link}
+							<li class="sidebar-navigation-item">
+								<div class="sidebar-navigation-itemWrapper">
+									<div class="sidebar-navigation-itemInnerWrapper">
+										<a
+											href={link.href}
+											class="relative m-0 flex grow cursor-pointer items-start rounded-lg px-2 py-1 text-left text-sm font-semibold text-gray-600 no-underline">
+											{link.name}
+										</a>
+									</div>
 								</div>
-							</div>
-						</li>
+							</li>
+						{/each}
 					</ul>
 				</div>
 			</div>
@@ -29,7 +44,7 @@
 	.scrollbar {
 		scrollbar-width: thin;
 		scrollbar-gutter: stable;
-		scrollbar-color: var(--color-gray-50) transparent;
+		scrollbar-color: var(--color-gray-true-400) transparent;
 		transition: scrollbar-color var(--motion-duration-100) var(--motion-ease-in);
 	}
 
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,12 +6,32 @@
 	let { children } = $props();
 </script>
 
-<div>
+<div class="flex h-screen flex-col">
 	<Header />
-	<div>
+	<div class="flex flex-1">
 		<Sidebar />
-		<main class="bg-gray-25 flex-1">
-			{@render children()}
+		<main class="bg-brand-700 mt-14 min-w-0 flex-1 pl-[var(--sidebar-width)]">
+			<div
+				class="scrollable-frame bg-gray-25 relative isolate h-screen h-svh max-h-none overflow-x-hidden overflow-y-auto rounded-tr-xl">
+				<div class="px-6">
+					{@render children()}
+				</div>
+			</div>
 		</main>
 	</div>
 </div>
+
+<style>
+	@supports (height: 100svh) {
+		.scrollable-frame {
+			height: calc(100svh - var(--header-height));
+		}
+	}
+
+	.scrollable-frame {
+		opacity: 1;
+		transition: opacity 50ms;
+		scrollbar-width: thin;
+		scrollbar-color: var(--color-gray-true-400) transparent;
+	}
+</style>
